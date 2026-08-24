fcb80728c3bb469053192c3a741812d10fdcfb37
EA - Jaiel Santos Cardoso Filho <santos.jjaiel18@gmail.com>
2026-08-23 23:09:54 -0300

    Adiciona tela inicial do SIA-Mobile

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>SIA-Mobile</Text>
+      <Text style={styles.subtitulo}>Sistema de Almoxarifado</Text>
+
+      <View style={styles.botao}>
+        <Text style={styles.textoBotao}>Ir para Login</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +16,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00f4fc',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titulo: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  subtitulo: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  botao: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  textoBotao: {
+    color: 'white',
+    fontSize: 16,
   },
 });
